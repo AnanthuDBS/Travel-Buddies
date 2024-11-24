c6df22b6fac9a1dd8f852735084b5ee856c480c1
AnanthuDBS <20033258@mydbs.ie>
2024-11-24 21:04:03 +0000

    trips.js debug and comments

--- backend/routes/trips.js
+++ backend/routes/trips.js
@@ -1,39 +1,45 @@
 //API routes for Creating, Reading/Fetching, Updating/Joining and Deleting the trips
 
+//dependencies import
 const express = require("express");
-const Trip = require("../models/trip");
-const router = express.Router():
+const Trip = require("../models/trip"); //Trip model to interact with the trips data in MongoDB
+const router = express.Router(); //new Express router for handling diff routes
 
 
 //for Creating a new Trip
+//Post /API/trips
 router.post("/", async (req, res) => {
     try {
-        const trip = new Trip(req.body);
-        await trip.save();
-        res.status(201).json(trip);
+        const trip = new Trip(req.body); //Create a new trip using the data from the req body
+        await trip.save(); //save the newly created trip to the DB
+        res.status(201).json(trip); //success case and response
     }   catch (err) {
-        res.status(400).json({error: err.message});
+        res.status(400).json({error: err.message}); //in case of invalid data
     }
-})
+});
 
 
 //To Fetch all created trips
+//GET /API/trips - POST changes to GET and no need of body
 router.get("/", async (req, res) => {
     try {
-        const trips = await Trip.find();
+        const trips = await Trip.find(); //retrieve all trips from DB
+        res.status(200).json(trips); //to respond with the loist of trips that are already created
     }   catch (err) {
         res.status(500).json({error: err.message });
     }
-})
+});
 
 // Delete a trip 
+//DELETE /API/trips/:id
 router.delete("/:id", async (req, res) => {
     try {
-        await Trip.findByIdAndDelete(req.params.id);
+        await Trip.findByIdAndDelete(req.params.id); //search and DEL the corresponding tripById by using 'id' param from the URL
         req.status(200).json({ message: "The Trip is deleted successfully."})
     }   catch (err) {
         res.status(500).json({error: err.message });
     }
 });
 
+//Export the routes to be used in the main app
 module.exports = router;
