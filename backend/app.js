6730d6acc6baf34fb976f56b5e5de7d633b11b4e
AnanthuDBS <20033258@mydbs.ie>
2024-11-24 00:56:10 +0000

    MongoDB connection

--- backend/app.js
+++ backend/app.js
@@ -6,3 +6,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//Connecting to mongoDB
+const MONGO_URI = "mongodb+srv://<username>:<password>@cluster.mongodb.net/travel-buddies?retryWrites=true&w=majority";
+
+mongoose
+    .connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("Connection to MongoDB successful"))
+    .catch((err) => console.error("Error while connecting to MongoDB:", err));
+
+
+//setting up Routes
