c6f436509b1590dcba0052e2ba89c2a339d0aa49
AnanthuDBS <20033258@mydbs.ie>
2024-11-24 21:47:21 +0000

    debug schema -round brackets to curly for datatype

--- backend/models/trip.js
+++ backend/models/trip.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 //define the schema for a trip using mongoose's schema class
 const tripSchema = new mongoose.Schema({
-    destination: (type:String, required:true),
-    modeOfTravel: (type:String, require:true),
-    timeOfTravel: (type:Date, required:true)
+    destination: {type:String, required:true},
+    modeOfTravel: {type:String, require:true},
+    timeOfTravel: {type:Date, required:true}
 });
 
 //Create a mongoose model for the 'Trip" collection using the tripSchema, this allows us to interact with the trips collection in mongoDB.
