91ee8054a82aa69fc08d7e2043724f8443e10bda
AnanthuDBS <20033258@mydbs.ie>
2024-11-24 21:45:04 +0000

    DB schema

---
+++ backend/models/trip.js
@@ -0,0 +1,12 @@
+//import mongoose module to work wiht MongoDB
+const mongoose = require("mongoose");
+
+//define the schema for a trip using mongoose's schema class
+const tripSchema = new mongoose.Schema({
+    destination: (type:String, required:true),
+    modeOfTravel: (type:String, require:true),
+    timeOfTravel: (type:Date, required:true)
+});
+
+//Create a mongoose model for the 'Trip" collection using the tripSchema, this allows us to interact with the trips collection in mongoDB.
+module.exports = mongoose.model("Trip", tripSchema)
