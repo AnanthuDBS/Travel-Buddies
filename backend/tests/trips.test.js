f3dee6c61dc8c9091e1be35207789a052c83f4a9
ANANTHU P SANKAR <ananthupsankar@gmail.com>
2024-12-11 20:02:41 +0000

    test to retrieve all trips

--- backend/tests/trips.test.js
+++ backend/tests/trips.test.js
@@ -6,6 +6,10 @@ const app = require("../app"); //to import my express app and server both
 const Trip = require("../models/trip"); //to import the Trip model
 
 describe("Trip API", () => {
+    //to clear and populate the db before tests
+    befireEach(async () => {
+        await Trip.deleteMany(); //to clr all the trips
+    });
     it("should create a new trip", async () => {
       const tripData = {
         destination: "Galway",
@@ -22,4 +26,29 @@ describe("Trip API", () => {
       const tripInDb = await Trip.findOne({ destination: "Galway" });
       expect(tripInDb).not.toBeNull();
     });
-  });
+
+    it("should retrieve all the trips", async() => {
+        //fill the db with sample trips
+        const trips = [
+            {
+                destination: "Cork",
+                modeOfTravel: "Train",
+                travelTime: "2024-12-15T08:00",
+                participantLimit: 5,
+            },
+            {
+                destination: "Kildare",
+                modeOfTravel: "Car",
+                travelTime: "2024-12-18T09:00",
+                participantLimit: 3,
+            },
+        ];
+        await Trip.insertMany(trips);
+
+        const res = await request(app).get("/api/trips");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.length).toBe(2);
+        expect(res.body[0].destination).toBe("Dublin");
+        expect(res.body[1].destination).toBe("Kildare");
+    });
+});
