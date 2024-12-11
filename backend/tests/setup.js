e245632b260fc0dfbaa4f3710347f8c4c146ffcd
ANANTHU P SANKAR <ananthupsankar@gmail.com>
2024-12-11 15:42:46 +0000

    update to the mock db and package.json to incl the setup fil

--- backend/tests/setup.js
+++ backend/tests/setup.js
@@ -12,3 +12,17 @@ beforeAll(async () => {
   await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
+// After all tests, disconnect and stop the server
+afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoServer.stop();
+  });
+  
+  // Clear the database before each test
+  beforeEach(async () => {
+    const collections = mongoose.connection.collections;
+    for (const key in collections) {
+      await collections[key].deleteMany({});
+    }
+  });
+  
